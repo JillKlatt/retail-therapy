3d477c2255f718971a9d284f5001fc8570f3db4e
JillKlatt <klatt.jill@gmail.com>
2021-06-17 13:06:52 -0400

    Import provider and create store from react redux and redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
