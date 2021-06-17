671dcc0a9076a5c153110f2e7ae24e638b746cbc
JillKlatt <klatt.jill@gmail.com>
2021-06-17 13:50:58 -0400

    Remove logo for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import pageReducer from './components/reducers/pageReducer';
+import Welcome from './components/Pages/Welcome'
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const [page, setPage] = useState("welcome")
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <pageReducer />
+    //     <div>
+    //     <nav>
+    //         <button onClick={() => setPage("welcome")}>Welcome</button>
+    //         <button onClick={() => setPage("game")}>Let's Play!</button>
+    //         <button onClick={() => setPage("leaderboard")}>View Leaderboard</button>
+    //         <button onClick={() => setPage("about")}>About</button>
+    //     </nav>
+    //     <div>
+    //         {pageReducer()}
+    //     </div>
+    // </div>
+    // )
+        
       </header>
     </div>
   );
