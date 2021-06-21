cfb023d23ca1c68f8c4a80cb89500108e88bf0ca
JillKlatt <klatt.jill@gmail.com>
2021-06-20 22:54:41 -0400

    loadLeaders doesnt' work yet, but take two

--- src/components/Pages/Leaderboard.js
+++ src/components/Pages/Leaderboard.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { getLeaders } from '../Actions/index'
+import { manageLeaders } from '../Reducers/manageLeaders'
 
 function Leaderboard() {
+    // console.log()
+
+    function loadLeaders() {
+        const leaders = getLeaders(dispatch)
+        // const leaders = manageLeaders(dispatch)
+        // debugger
+        leaders.map(<li>{u => u.username}</li>)
+    }
 
 
     return (
         <div>
             Leaderboard!
+            <ul>
+                {leaders}
+            </ul>
         </div>
     )
 } 
