82a3e0b6ef358d4516682b01c64def9a279e36f6
JillKlatt <klatt.jill@gmail.com>
2021-06-21 15:02:46 -0400

    Remove manageLeaders func for now

---
+++ notes.md
@@ -0,0 +1,6 @@
+
+## 6/21
+[ ] Grab data from database
+[ ] Organize data in point order
+[ ] Post new data to the backend and have it immediately render
+[ ] Remove nameInput after user submits name
--- src/components/Reducers/manageLeaders.js
+++ src/components/Reducers/manageLeaders.js
@@ -1,13 +1,32 @@
-export default function manageLeaders(state = {
-    leaders: []
-}, action) {
+// function manageLeaders(state = {
+//     leaders: []
+// }, action) {
+//     switch (action.type){
+//         case 'GET_LEADERS':
+//             console.log(action.payload)
+//             return {
+//                 ...state,
+//                 leaders: [...state.leaders, ...action.payload]
+//             }
+//         default:
+//             return state
+// }}
+
+// export default manageLeaders()
+
+export default function manageLeaders(state = {leaders: []}, action){
     switch (action.type){
         case 'GET_LEADERS':
-            console.log(action.payload)
+            console.log(action)
             return {
-                ...state,
-                leaders: action.payload
+                ...state, 
+                leaders: [...state.leaders, ...action.payload]
+        
             }
+
         default:
             return state
-}}
+    }
+
+}
+
