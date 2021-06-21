91cc57154c7a25f5343c7e30de7a96dcf06e04ba
JillKlatt <klatt.jill@gmail.com>
2021-06-21 16:55:13 -0400

    Add submitGame function to submit user and points after game is complete (post request)

--- src/components/Pages/Game.js
+++ src/components/Pages/Game.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+// import { submitGame } from '../Actions/index'
+
 
 function Game() {
 
+
+    const state = useSelector((state) => state)
+    console.log(state.user)
+
     function submitGame() {
+        debugger
+        fetch("http:///127.0.0.1:3001/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify(state.user),
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
 
     }
+    // function finishGame() {
+    //     submitGame()
+    // }
 
 
     return (
