357bfa693c9781c5f14e2f00e6f2820138e1c117
JillKlatt <klatt.jill@gmail.com>
2021-06-20 22:55:55 -0400

    Add leaders to initial state

--- src/components/Reducers/index.js
+++ src/components/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import manageUser from './manageUser'
+import manageLeaders from './manageLeaders'
 
 const reducers = combineReducers({
     //key value pair
-    user: manageUser
+    user: manageUser,
+    leaders: manageLeaders
 })
 
 export default reducers
