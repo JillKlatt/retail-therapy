d479083b4d0acf4c898ec5b9f2e9fee71f56b191
JillKlatt <klatt.jill@gmail.com>
2021-06-21 15:01:31 -0400

    Add button to submit score to game

--- src/components/Pages/Game.js
+++ src/components/Pages/Game.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 function Game() {
 
+    function submitGame() {
+
+    }
+
 
     return (
         <div>
-            Game!
+            Here's where the game will go! Woooo! <br></br>
+            <button onClick={submitGame}>Submit Score!</button>
         </div>
     )
 } 
