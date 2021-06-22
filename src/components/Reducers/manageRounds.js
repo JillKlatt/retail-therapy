381c305a242f64dfdafd39280b3c5f8c02bc1b4b
JillKlatt <klatt.jill@gmail.com>
2021-06-22 18:06:07 -0400

    Change round to update +1 every turn

--- src/components/Reducers/manageRounds.js
+++ src/components/Reducers/manageRounds.js
@@ -1,15 +1,16 @@
-export default function manageRounds(state = 0, action){
+export default function manageRounds(state = {round: 0}, action){
     switch (action.type){
         case 'CHANGE_ROUND':
             console.log(action)
             return {
-                ...state, 
-                round: [action.payload]
-        
-            }
+                    ...state,
+                    round: state.round + 1
+                }
 
         default:
             return state
     }
 
 }
+
+//                round: [state.round, action.payload]
