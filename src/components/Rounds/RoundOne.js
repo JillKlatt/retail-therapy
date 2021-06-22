08cbf806822d1bbae9932956b6557629a0c321ea
JillKlatt <klatt.jill@gmail.com>
2021-06-22 15:03:43 -0400

    Add variables for rendering rounds

---
+++ src/components/Rounds/RoundOne.js
@@ -0,0 +1,12 @@
+import Villain from '../Player/Villain/Villain'
+import Card from '../Player/Card/Card'
+
+const RoundOne =  (
+    <div>    
+    <Villain name={"Villain Number One!"}/>
+    <Card choice={"Run Away!"}/>
+    <Card choice={"Fight!"}/>
+    </div>
+
+)
+export default RoundOne
