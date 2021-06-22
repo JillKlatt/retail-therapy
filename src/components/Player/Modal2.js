7101f65c27203547e40f21d67445f42697544837
JillKlatt <klatt.jill@gmail.com>
2021-06-22 17:07:38 -0400

    Add Modals but will delete, don't work

---
+++ src/components/Player/Modal2.js
@@ -0,0 +1,16 @@
+
+
+import Modal from '@material-ui/core/Modal';return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
