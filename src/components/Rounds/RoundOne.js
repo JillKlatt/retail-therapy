2061c26069bf25c571c18fb9dc7ac149c3adce49
JillKlatt <klatt.jill@gmail.com>
2021-06-22 17:46:08 -0400

    Change to functional to prop drill, will remove now using dispatch

--- src/components/Rounds/RoundOne.js
+++ src/components/Rounds/RoundOne.js
@@ -3,17 +3,17 @@ import Card from '../Player/Card/Card'
 
 //somewhere inside here, change round to two!
 
-const RoundOne = () => {
+function RoundOne(props) {
 
-    function changeRound() {
-        console.log(this.props)
-        this.props.setRound()
-    }
+    // function changeRound() {
+    //     console.log(this.props)
+    //     this.props.setRound()
+    // }setRound={changeRound}/>setRound={changeRound}
     return (
     <div>    
     <Villain name={"Villain Number One!"}/>
-    <Card choice={"Run Away!"} outcome={"You Ran Away!"} setRound={changeRound}/>
-    <Card choice={"Fight!"} outcome={"Stay And Fight!"} setRound={changeRound}/>
+    <Card choice={"Run Away!"} outcome={"You Ran Away!"} setCurrentRound={props.setCurrentRound}/> 
+    <Card choice={"Fight!"} outcome={"Stay And Fight!"} setCurrentRound={props.setCurrentRound}/>
     </div>
     )
 }
