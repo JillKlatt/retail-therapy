b7a88eb737436cf5bb35c6a699d55e3b382f6a49
JillKlatt <klatt.jill@gmail.com>
2021-06-22 17:12:58 -0400

    Add rounds to store state

--- src/components/Reducers/index.js
+++ src/components/Reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import manageUser from './manageUser'
 import manageLeaders from './manageLeaders'
+import manageRounds from './manageRounds'
 
 const reducers = combineReducers({
     //key value pair
     user: manageUser,
-    leaders: manageLeaders
+    leaders: manageLeaders,
+    round: manageRounds
 })
 
 export default reducers
