29fd5f260a55315244294cae9bbf4284370c7fdc
JillKlatt <klatt.jill@gmail.com>
2021-06-22 17:09:24 -0400

    Add alert for rendered outcome

--- src/components/Player/Card/Card.js
+++ src/components/Player/Card/Card.js
@@ -1,16 +1,33 @@
 import React from 'react';
+// import { Modal } from '@material-ui/core/Modal';
+// import SimpleModal from '../Modal'
 
 class Card extends React.Component {
+
 
     constructor(props){
         super(props);
         this.choice = props.choice
+        this.outcome = props.outcome
+        this.setRound = props.changeRound
     }
+
+    // function changeRound() {
+    //     console.log(this.props)
+    //     this.props.setRound("2")
+    // }
+
+
+    renderOutcome = () => {
+        // debugger
+        alert(this.outcome)
+        this.setRound("2")
+        }
 
     render(){
         return(
             <div>
-                {this.choice}
+                <button onClick={this.renderOutcome}>{this.choice}</button>
             </div>
 
         )
