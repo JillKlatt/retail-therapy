6cbed26c7b3e6d0572cac03e47a995997dfb413e
JillKlatt <klatt.jill@gmail.com>
2021-06-22 17:08:45 -0400

    Create RoundOne to render villian and two cards

--- src/components/Rounds/RoundOne.js
+++ src/components/Rounds/RoundOne.js
@@ -1,12 +1,21 @@
 import Villain from '../Player/Villain/Villain'
 import Card from '../Player/Card/Card'
 
-const RoundOne =  (
+//somewhere inside here, change round to two!
+
+const RoundOne = () => {
+
+    function changeRound() {
+        console.log(this.props)
+        this.props.setRound()
+    }
+    return (
     <div>    
     <Villain name={"Villain Number One!"}/>
-    <Card choice={"Run Away!"}/>
-    <Card choice={"Fight!"}/>
+    <Card choice={"Run Away!"} outcome={"You Ran Away!"} setRound={changeRound}/>
+    <Card choice={"Fight!"} outcome={"Stay And Fight!"} setRound={changeRound}/>
     </div>
+    )
+}
 
-)
 export default RoundOne
