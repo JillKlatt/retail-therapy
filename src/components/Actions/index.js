1bcc2168fb3220f7d12fdc4dcc5d2dbf8326f0a0
JillKlatt <klatt.jill@gmail.com>
2021-06-23 17:34:55 -0400

    Remove functions from object actions

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -1,28 +1,21 @@
 export const createUser = (userInput) => {
-    return (dispatch) => {
-        // debugger
-        dispatch({
-            type: "ADD_USER",
-            payload: userInput
-        })
+    return {
+        type: "ADD_USER",
+        payload: userInput
     }
 }
 
 export const addPoint = (amount) => {
-    return (dispatch) => {
-        dispatch ({
-            type: "ADD_POINT",
-            payload: amount
-        })
+    return{
+        type: "ADD_POINT",
+        payload: amount
     }
 }
 
 export const changeRound = (round) => {
-    return (dispatch) => {
-        dispatch ({
-            type: "CHANGE_ROUND",
-            payload: round
-        })
+    return {
+        type: "CHANGE_ROUND",
+        payload: round
     }
 }
 
