985a68667cbf61485d07d520baec7f9c7c89e6b1
JillKlatt <klatt.jill@gmail.com>
2021-06-23 15:55:55 -0400

    Add point functionality to each round

--- src/components/Rounds/RoundOne.js
+++ src/components/Rounds/RoundOne.js
@@ -2,9 +2,10 @@ import Villain from '../Player/Villain/Villain'
 import Card from '../Player/Card/Card'
 import './Round.css'
 
-//somewhere inside here, change round to two!
+
 
 function RoundOne(props) {
+
 
     return (
     <div className='round'>    
@@ -12,8 +13,8 @@ function RoundOne(props) {
             <Villain name={"Villain Number One!"} description={"V Scary"}/>
         </div>
         <div className='card-row'>
-            <Card choice={"This is too scary!"} buttonChoice={"Run Away!"} outcome={"You Ran Away!"} setCurrentRound={props.setCurrentRound}/> 
-            <Card choice={"You can take em!"} buttonChoice={"Fight!"} outcome={"Stay And Fight!"} setCurrentRound={props.setCurrentRound}/>
+            <Card answer={"incorrect"} choice={"Wrong Answer: Test One"} buttonChoice={"Run Away!"} outcome={"You Ran Away!"} setCurrentRound={props.setCurrentRound}/> 
+            <Card answer={"correct"} choice={"This is the right answer: Test One"} buttonChoice={"Fight!"} outcome={"Stay And Fight!"} setCurrentRound={props.setCurrentRound}/>
         </div>
     </div>
     )
