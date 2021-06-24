1a1e7ccaa9d0c6f92ce58ca729a0fc2d5df6c4da
JillKlatt <klatt.jill@gmail.com>
2021-06-24 15:46:07 -0400

    Remove sorting logic

--- src/components/Pages/Leaderboard.js
+++ src/components/Pages/Leaderboard.js
@@ -19,7 +19,6 @@ function Leaderboard() {
 
     function renderLeaders(){
         // Move to backend
-        // const sortedLeaders = leaders.sort((a, b) => (a.points < b.points) ? 1 : -1)
         
         return (leaders.map((leader, index) => <li key={index}>{leader.username} - {leader.points}</li>)
         )}
