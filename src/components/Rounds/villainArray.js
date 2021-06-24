5a0c4631fb17778538a1ef759521703b22421b4e
JillKlatt <klatt.jill@gmail.com>
2021-06-24 15:46:49 -0400

    Create villain array

---
+++ src/components/Rounds/villainArray.js
@@ -0,0 +1,17 @@
+export const villainArray = [
+    {
+        name: "Round Zero!",
+        description: ""
+    },
+    {
+        name: "Villain Number One!",  
+        description: "V Scary"
+    },
+    {
+        name: "Villain Number Two!",  
+        description: "V Scary"
+    },
+    {
+        name: "Here is where our story begins! There will be backstory and info and wooooo"
+    }
+    ]
