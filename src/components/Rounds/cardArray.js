0ae77693103d9afd0c8a5d8e2655f891d074f09a
JillKlatt <klatt.jill@gmail.com>
2021-06-24 15:46:25 -0400

    Create card array

---
+++ src/components/Rounds/cardArray.js
@@ -0,0 +1,20 @@
+export const cardArray = [
+    {
+        answer: "incorrect",
+        choice: "Let's Fucking Go Bro!",
+        buttonChoice: "Start Game",
+        outcome: "Here We Go!"
+    },
+    {
+        answer:"incorrect", 
+        choice: "Wrong Answer: Test One", 
+        buttonChoice: "Run Away!", 
+        outcome: "You Ran Away!"
+    },
+    {
+        answer:"correct", 
+        choice: "Right Answer: Test One", 
+        buttonChoice: "Fight!", 
+        outcome: "Fight!"   
+    }
+]
