72062b27b4d755c2c608a416b2708214647169f4
JillKlatt <klatt.jill@gmail.com>
2021-06-24 13:38:28 -0400

    Create new component for header"

---
+++ src/components/Pages/Header.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
+
+import Leaderboard from './Leaderboard';
+import Game from './Game';
+import About from './About'
+// import Form from '../Form'
+
+function Header() {
+
+    
+
+    return (
+        <div>
+            Retail Therapy!
+        </div>
+
+    )
+} 
+
+export default Header;
