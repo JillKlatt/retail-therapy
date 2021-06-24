8b93151b824770b5e9e840d33a77ceb0bd0e8bd3
JillKlatt <klatt.jill@gmail.com>
2021-06-24 17:27:36 -0400

    Add to villain array

--- src/components/Rounds/villainArray.js
+++ src/components/Rounds/villainArray.js
@@ -10,8 +10,5 @@ export const villainArray = [
     {
         name: "Villain Number Two!",  
         description: "V Scary"
-    },
-    {
-        name: "Here is where our story begins! There will be backstory and info and wooooo"
     }
     ]
