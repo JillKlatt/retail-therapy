ce3275348c659323c8bcbe5fa499947a041e8685
JillKlatt <klatt.jill@gmail.com>
2021-06-24 23:06:27 -0400

    Add second card for choices

--- src/components/Rounds/Round.js
+++ src/components/Rounds/Round.js
@@ -15,7 +15,8 @@ function Round(props) {
 
         </div>
         <div className='card-row'>
-            <Card answer={props.cards.answer} choice={props.cards.choice} buttonChoice={props.cards.buttonChoice} outcome={props.cards.outcome} />
+            <Card answer={props.rightCard.answer} choice={props.rightCard.choice} buttonChoice={props.rightCard.buttonChoice} outcome={props.rightCard.outcome} />
+            <Card answer={props.leftCard.answer} choice={props.leftCard.choice} buttonChoice={props.leftCard.buttonChoice} outcome={props.leftCard.outcome} />
         </div>
     </div>
     )
