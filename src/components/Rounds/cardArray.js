24d115d6d9963fcd016453cf4cba5a4e32cf1f1e
JillKlatt <klatt.jill@gmail.com>
2021-06-24 23:06:48 -0400

    Add second array for second card array

--- src/components/Rounds/cardArray.js
+++ src/components/Rounds/cardArray.js
@@ -1,4 +1,4 @@
-export const cardArray = [
+export const rightCardArray = [
     {
         answer: "incorrect",
         choice: "Let's Fucking Go Bro!",
@@ -13,7 +13,28 @@ export const cardArray = [
     },
     {
         answer:"correct", 
-        choice: "Right Answer: Test One", 
+        choice: "Both Right!", 
+        buttonChoice: "Fight!", 
+        outcome: "Fight!"   
+    }
+]
+
+export const leftCardArray = [
+    {
+        answer: "correct",
+        choice: "Let's Fucking Go Bro!",
+        buttonChoice: "Start Game",
+        outcome: "Here We Go!"
+    },
+    {
+        answer:"correct", 
+        choice: "Right Answer", 
+        buttonChoice: "Run Away!", 
+        outcome: "You Ran Away!"
+    },
+    {
+        answer:"correct", 
+        choice: "Both Right!", 
         buttonChoice: "Fight!", 
         outcome: "Fight!"   
     }
