09353cefc5f1ba94aa210fc5efa16300660d16a3
JillKlatt <klatt.jill@gmail.com>
2021-06-26 16:50:56 -0400

    Create about card

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -5,8 +5,14 @@ function About() {
 
     return (
         <div>
-            Here's where the about will go! <br></br>
-            Wow! So much info! Who knew!
+            <div className="about-header">
+            <h3>Welcome to Retail Therapy, the interactive adventure! </h3>
+            </div>
+            <div className='about-body'>
+            <p>For those brave enough to accept the challenge and horrible wages, </p>
+            <p>Your manager, who was once a beacon of hope and joy and light, has been put under some sort of spell by their boss, cursing her to become glum and irritated and overwhelmed. Your task is to complete the challenges and obstacles of an hourly employee to defeat the evil DM (District Manager)</p>
+            <p>You will be presented with a choice, be warned, there is usually a right answer, but not always.</p>
+            </div>
         </div>
     )
 } 
