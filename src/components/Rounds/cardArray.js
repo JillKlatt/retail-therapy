04040612dedad9a9947e0f31c93e1f5b66937d66
JillKlatt <klatt.jill@gmail.com>
2021-06-28 15:15:41 -0400

    Update cardarray

--- src/components/Rounds/cardArray.js
+++ src/components/Rounds/cardArray.js
@@ -1,9 +1,16 @@
 export const rightCardArray = [
     {
+        answer:"correct", 
+        choice: "Stand Your Ground, it's your day off, you're basically a customer too", 
+        buttonChoice: "Intimiation!", 
+        outcome: "You stare her down as she locks eyes with you, but at the last moment course-corrects and veers to her right"
+    },
+    {
         answer:"incorrect", 
-        choice: "Ignore Her", 
-        buttonChoice: "Go check the cabinets", 
-        outcome: "You Ran Away!"
+        hp: 10,
+        choice: "Ignore Her and go check the cabinets, maybe there's something that will help there", 
+        buttonChoice: "Investigate!", 
+        outcome: "In the cabinets you find unrefrigerated condiments and an open offbrand Redbull "
     },
     {
         answer:"incorrect", 
@@ -21,10 +28,16 @@ export const rightCardArray = [
 
 export const leftCardArray = [
     {
+        answer:"incorrect", 
+        choice: "You're barely in the door, it's not worth making a scene in the vestibule with some stranger. Dodge out of the way just in time.", 
+        buttonChoice: "Dexterity!", 
+        outcome: "You attempt to leap to your right to avoid the oncoming cart, but the customer sees your movement and redirects her attack towards you "
+    },
+    {
         answer: "correct",
-        choice: "Ask About Her Cats!",
-        buttonChoice: "Look at her phone",
-        outcome: "Here We Go!"
+        choice: "Ask her about her cats",
+        buttonChoice: "Persuasion!",
+        outcome: "Lisa regales you with trivia and pictures of her multiple cats. In between this information, she mentions she saw the manager in the stockroom."
     },
     {
         answer:"correct", 
