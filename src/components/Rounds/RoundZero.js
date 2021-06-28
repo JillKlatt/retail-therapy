5233f7c9b4c846ff3815f16479473119f4075c67
JillKlatt <klatt.jill@gmail.com>
2021-06-28 15:16:25 -0400

    Add more story to the first round

--- src/components/Rounds/RoundZero.js
+++ src/components/Rounds/RoundZero.js
@@ -6,7 +6,7 @@ function RoundZero(props) {
 
     return (
     <div>    
-    <Card choice={"Here is where our story begins! There will be backstory and info and wooooo"} buttonChoice={"Begin Game!"} outcome={"Here We Go!"} setCurrentRound={props.setCurrentRound}/> 
+    <Card choice={"You find yourself transported via portal to just outside your local Discount Grocery Store. By choosing one of the provided options, navigate through the store to accomplish tasks and eventually save your manager from the Big Boss"} buttonChoice={"Begin Game!"} outcome={"Here We Go!"} setCurrentRound={props.setCurrentRound}/> 
     </div>
     )
 }
