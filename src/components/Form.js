24b7feec51a3ad055e671546180e77fe19a3eefd
JillKlatt <klatt.jill@gmail.com>
2021-06-28 14:34:41 -0400

    Change input format to styled components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,6 @@
 import { useState } from 'react' 
+import '../App.css'
+import styled from 'styled-components'
 
 function UsernameInput(props) {
 
@@ -15,11 +17,33 @@ function UsernameInput(props) {
         props.setInput(true)
     }
 
+    // const Input = styled.input`
+    //     padding: 0.5em;
+    //     margin: 0.5em;
+    //     background: papayawhip;
+    //     border: none;
+    //     border-radius: 3px;
+    // `;
+
+    // const FeatureButton = styled.button`
+    //     background: transparent;
+    //     font-family:Verdana, Geneva, Tahoma, sans-serif;
+    //     border-radius: 3px;
+    //     border: 2px solid black;
+    //     color: blue;
+    //     margin: 0 1em;
+    //     padding: 0.25em 1em;
+    //     &:hover{
+    //         color: red;
+
+    //     }
+    // `;
+
 
     return (
         <div>
-            <input name="username" value={username} onChange={onChange}/>
-            <input onClick={handleSubmit} type="submit" value="Send to HR"/>
+            <input name="username" type='text' value={username} onChange={onChange}/>
+            <input onClick={handleSubmit} type="submit" value="Send to HR"></input>
         </div>
     )
 
