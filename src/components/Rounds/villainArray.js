174719335274c41a739e4cd0a5e4060312721cb0
JillKlatt <klatt.jill@gmail.com>
2021-06-28 15:15:20 -0400

    Update villainarray

--- src/components/Rounds/villainArray.js
+++ src/components/Rounds/villainArray.js
@@ -1,8 +1,11 @@
 export const villainArray = [
-
+    {
+        name: "You Arrive..",
+        description: "You are met with signs advertising that 'Heroes Work Here', a familiar sign in these uncertain times. A thought crosses your mind, if you were truly heros, perhaps you'd get hazard pay. Huh. Large magnificent doors slide open with arcane power, sensing your presence. A demoic looking customer is quickly approaching you with her cart, making no attempt to serve out of the way."
+    },
     {
         name: "You enter the breakroom",  
-        description: "You see a small, yet very fancy breakroom. There are cabinets along the walls, a couple tables, and for some reason, a television bigger than the one in Jill’s house. You see a small halfling that you recognize as Lisa. She’s small, even for a halfling, her feet don’t touch the floor as she sits at one of the tables. She looks up from her phone and goes, Hey! You don't work today!"
+        description: `You see a small, yet very fancy breakroom. There are cabinets along the walls, a couple tables, and for some reason, a television bigger than the one in Jill’s house. You see a small halfling that you recognize as Lisa. She’s small, even for a halfling, her feet don’t touch the floor as she sits at one of the tables. She looks up from her phone and goes, "Hey! You don't work today!"`
     },    
     {
         name: "Round Zero!",
