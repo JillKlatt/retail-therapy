cf388196c2e65e7c67dbb4c9397e59d8bfc638ee
JillKlatt <klatt.jill@gmail.com>
2021-06-29 16:41:00 -0400

    Remov console log

--- src/components/Reducers/manageRounds.js
+++ src/components/Reducers/manageRounds.js
@@ -1,7 +1,6 @@
 export default function manageRounds(state = {round: 0}, action){
     switch (action.type){
         case 'CHANGE_ROUND':
-            console.log(action)
             return {
                     ...state,
                     round: state.round + 1
