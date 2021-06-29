bbee1b3f92a88395d87adfc1130a60c397fe0f1f
JillKlatt <klatt.jill@gmail.com>
2021-06-29 16:42:18 -0400

    Add changeHP reducer

--- src/components/Reducers/manageUser.js
+++ src/components/Reducers/manageUser.js
@@ -1,6 +1,7 @@
 export default function manageUser(state = {
     username: "",
     points: 0,
+    health: 50
 }, action) {
     switch (action.type){
         case 'ADD_USER':
@@ -12,13 +13,19 @@ export default function manageUser(state = {
         case 'ADD_POINT':
             return {
                 ...state,
-                points: state.points + 1
+                points: state.points + 10
             }
 
         case 'RESET_POINTS':
             return {
                 ...state,
                 points: 0
+            }
+
+        case 'CHANGE_HP':
+            return {
+                ...state,
+                health: state.health + action.payload
             }
 
         default:
