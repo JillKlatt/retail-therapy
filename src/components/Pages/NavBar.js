cef508a92a15f6e24bb1f03dc91b6c6651d1c993
JillKlatt <klatt.jill@gmail.com>
2021-06-30 13:29:47 -0400

    Change NavbarLink to NavLink to avoid error of double a tags

--- src/components/Pages/NavBar.js
+++ src/components/Pages/NavBar.js
@@ -1,31 +1,18 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import ScoreCard from './ScoreCard'
 import './Page.css'
-import { Navbar, NavbarLink } from 'styled-navbar-component';
-import { Nav } from 'styled-nav-component';
 
 function NavBar(){
 
     return(
         <div>
-        {/* <Navbar expandXL> */}
-        <Nav start>
         <div id='nav-bar' className='navbar'>
-            <Link to='/game'>
-                <NavbarLink id='game'>Let's Play</NavbarLink>
-            </Link>
-            <Link to='/leaderboard'>
-                <NavbarLink light id='leaderboard'>Leaderboard</NavbarLink>
-            </Link>
-            <Link to='/about'>
-                <NavbarLink light id='about'>About</NavbarLink>
-            </Link>
-   </div>
-            </Nav>
-
-        {/* </Navbar> */}
+            <NavLink to='/game' activeStyle={{fontWeight: "bold"}} id='game'>Let's Play</NavLink>
+            <NavLink to='/leaderboard' activeStyle={{fontWeight: "bold"}} id='leaderboard'>Leaderboard</NavLink>
+            <NavLink to='/about'  activeStyle={{fontWeight: "bold"}} id='about'>About</NavLink>
+        </div>
             <ScoreCard />
         </div>
     )
