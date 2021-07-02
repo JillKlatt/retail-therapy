3241d0afb0ac47976b0f99c1b1f278211ef0e422
JillKlatt <klatt.jill@gmail.com>
2021-07-02 10:34:39 -0400

    Create playAgain function and button for last round

--- src/components/Rounds/LastRound.js
+++ src/components/Rounds/LastRound.js
@@ -7,11 +7,17 @@ function LastRound(props) {
         props.submitGame()
     }
 
+    function reset() {
+        props.playAgain()
+    }
+
 
     return (
     <div>    
-    <Villain name={"Way to Go!"}/>
-    <Link onClick={finishGame} to='/leaderboard'> The Game is Over! Click here to submit your score!</Link>
+    <Villain name={"You freed your manager but more importantly, saved the world!"}/>
+    <Link onClick={finishGame} to='/leaderboard'>Click here to submit your score!</Link>
+    <br></br><br></br>
+    <button onClick={reset}>Think you can do better? Click here to start over</button>
     </div>
     )
 }
