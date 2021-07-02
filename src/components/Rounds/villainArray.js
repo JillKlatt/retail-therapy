51e700c61763a3b6035744404ff7efdcd80588d2
JillKlatt <klatt.jill@gmail.com>
2021-07-02 10:34:12 -0400

    Change prompts for assessment

--- src/components/Rounds/villainArray.js
+++ src/components/Rounds/villainArray.js
@@ -14,5 +14,17 @@ export const villainArray = [
     {
         name: "Back in the stockroom..",  
         description: "To your left is a stack of pallets that employees sometimes hide behind and to the right is the freezer, she could be anywhere"
+    },
+    {
+        name: "After finding your manager..",
+        description: "She informs you that your Big Boss is threatening to shut down the store in a very 'Evil Villain in a Hallmark Movie' type of way. Will you stay and fight?"
+    },
+    {
+        name: "You turn around to face your Big Boss..",
+        description: "For a moment you see a person with their own dreams and fears and flaws, and it almost breaks your heart. But then you remember when you had to come in at 2am after you just got off at 11 and had to be back at 6 just because it didn't want to drive all the way out there and that feeling fades as its visage becomes one of a giant dragon!!"
+    },
+    {
+        name: "After a few rounds of fighting..",
+        description: "You see a weak point in its stomach. If you strike now, you might defeat it!"
     }
     ]
