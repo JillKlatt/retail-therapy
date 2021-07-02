282d1f1d7984d71a9339b93766d478f018ff6410
JillKlatt <klatt.jill@gmail.com>
2021-07-02 12:01:02 -0400

    Remove green text

--- src/components/Reducers/manageLeaders.js
+++ src/components/Reducers/manageLeaders.js
@@ -1,30 +1,13 @@
-// function manageLeaders(state = {
-//     leaders: []
-// }, action) {
-//     switch (action.type){
-//         case 'GET_LEADERS':
-//             console.log(action.payload)
-//             return {
-//                 ...state,
-//                 leaders: [...state.leaders, ...action.payload]
-//             }
-//         default:
-//             return state
-// }}
-
-// export default manageLeaders()
 
 export default function manageLeaders(state = {leaders: []}, action){
     switch (action.type){
         case 'GET_LEADERS':
-            //console.log(`Action: ${action.payload}`)
             return {
                 ...state, 
                 leaders: action.payload
             }
 
         case 'ADD_LEADER':
-            // debugger
             return {
                 ...state,
                 leaders: [...state.leaders, action.payload]
