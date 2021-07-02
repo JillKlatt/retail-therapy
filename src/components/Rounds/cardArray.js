e903e1297d2fc9d2be8d3e44f911f771d3893567
JillKlatt <klatt.jill@gmail.com>
2021-07-02 10:34:57 -0400

    Change cards for assessment

--- src/components/Rounds/cardArray.js
+++ src/components/Rounds/cardArray.js
@@ -16,16 +16,34 @@ export const rightCardArray = [
     {
         answer:"incorrect", 
         hp: +10,
-        choice: "Ignore Her and go check the cabinets, maybe there's something that will help there", 
+        choice: "Ignore her and go check the cabinets, maybe there's something that will help there", 
         buttonChoice: "Investigate!", 
         outcome: "In the cabinets you find unrefrigerated condiments and an open offbrand Redbull, chugging it might make you feel better"
     },
     {
         answer:"correct", 
-        choice: "You know your manager can sometimes perfer the solitude of the freezer, no one bothers you in there", 
-        buttonChoice: "Go Right to the Freezer", 
+        choice: "You know your manager can sometimes prefer the solitude of the freezer, no one bothers you in there", 
+        buttonChoice: "Go Left to the Freezer", 
         outcome: "You found her!"   
-    }
+    },
+    {
+        answer:"incorrect", 
+        choice: "You don't get paid enough for this nonsense!", 
+        buttonChoice: "Flight!", 
+        outcome: "Coward! You have to stay and fight because this game doesn't have that type of choice functionality yet. But also, come on."   
+    },
+    {
+        answer:"correct", 
+        choice: "Take a broke piece of pallet and attempt to sweep its legs", 
+        buttonChoice: "Athletics!", 
+        outcome: "You knock it prone on the ground in front of you, now's your chance!"   
+    },
+    {
+        answer:"correct", 
+        choice: "Luckily the stockroom always comes equipped with a heavy crossbow. Take a step back and FIRE!", 
+        buttonChoice: "Ranged Attack!", 
+        outcome: "You hear your manager's cheers before anything else, you've defeated the evil DM!"   
+    },
 ]
 export const leftCardArray = [
     {
@@ -44,14 +62,33 @@ export const leftCardArray = [
     {
         lAnswer: "correct",
         lHp: 0,
-        lChoice: "Ask her about her cats",
+        lChoice: "Lisa could have a wealth of information if we can get on her good side.. Ask her about her cats",
         lButtonChoice: "Persuasion!",
         lOutcome: "Lisa regales you with trivia and pictures of her multiple cats. In between this information, she mentions she saw the manager in the stockroom."
     },
     {
         lAnswer:"correct", 
         lChoice: "Your manager isn't one to take breaks, but perhaps she's searching for something over there", 
-        lButtonChoice: "Go Left to check behind the pallets", 
+        lButtonChoice: "Go Right to check behind the pallets", 
         lOutcome: "You found her!"   
+    },
+    {
+        lAnswer:"correct", 
+        lChoice: "Enough is enough! Take her down once and for all!", 
+        lButtonChoice: "Fight!", 
+        lOutcome: "You're exactly what this world needs!!"   
+    },
+    {
+        lAnswer:"incorrect", 
+        lHp: -10,
+        lChoice: "This calls for fisticuffs", 
+        lButtonChoice: "Strength!", 
+        lOutcome: "All these hours of lifting banana boxes have made you strong, but not strong enough to punch a dragon. It sees it coming and reacts with fire breath!"   
+    },
+    {
+        lAnswer:"correct", 
+        lChoice: "You pull your box cutter from your pocket and charge for the weak spot!", 
+        lButtonChoice: "Melee Attack!", 
+        lOutcome: "You hear your manager's cheers before anything else, you've defeated the evil DM!"   
     }
 ]
