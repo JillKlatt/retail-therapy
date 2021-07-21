b47fcb45108956b5b25210abd1f4bff2786ce408
JillKlatt <klatt.jill@gmail.com>
2021-07-21 18:13:52 -0400

    Add reducer to index

--- src/components/Reducers/index.js
+++ src/components/Reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import manageUser from './manageUser'
 import manageLeaders from './manageLeaders'
 import manageRounds from './manageRounds'
+import manageInventory from './manageInventory'
 
 const reducers = combineReducers({
     //key value pair
     user: manageUser,
     leaders: manageLeaders,
-    round: manageRounds
+    round: manageRounds,
+    inventory: manageInventory
 })
 
 export default reducers
