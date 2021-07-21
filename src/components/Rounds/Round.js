be427320dae26baaee18ea67f49cd565ea0ae3dc
JillKlatt <klatt.jill@gmail.com>
2021-07-21 18:10:50 -0400

    Add inventory to props

--- src/components/Rounds/Round.js
+++ src/components/Rounds/Round.js
@@ -4,8 +4,8 @@ import './Round.css'
 
 const Round = ({villains, rightCard, leftCard}) => {
 
-    const { answer, buttonChoice, choice, hp, outcome } = rightCard
-    const { lAnswer, lButtonChoice, lChoice, lHp, lOutcome} = leftCard
+    const { answer, buttonChoice, choice, hp, outcome, inventory } = rightCard
+    const { lAnswer, lButtonChoice, lChoice, lHp, lOutcome, lInventory } = leftCard
     const { name, description } = villains
 
 
@@ -16,8 +16,8 @@ const Round = ({villains, rightCard, leftCard}) => {
 
         </div>
         <div className='card-row'>
-            <Card hp={hp} answer={answer} choice={choice} buttonChoice={buttonChoice} outcome={outcome} />
-            <Card hp={lHp} answer={lAnswer} choice={lChoice} buttonChoice={lButtonChoice} outcome={lOutcome} />
+            <Card hp={hp} answer={answer} choice={choice} buttonChoice={buttonChoice} outcome={outcome} inventory={inventory}/>
+            <Card hp={lHp} answer={lAnswer} choice={lChoice} buttonChoice={lButtonChoice} outcome={lOutcome} inventory={lInventory}/>
         </div>
     </div>
     )
