3494793514c272e5a672fee2603317ce40f01979
JillKlatt <klatt.jill@gmail.com>
2021-08-02 12:00:52 -0400

    Add 'remove from inventory' reducer

--- src/components/Reducers/manageInventory.js
+++ src/components/Reducers/manageInventory.js
@@ -1,10 +1,16 @@
 export default function manageInventory(state = {inventory: []}, action){
     switch (action.type){
         case "ADD_TO_INVENTORY":
-            return{
+            return {
             ...state,
             inventory: [...state.inventory, action.payload]
         }
+
+        case "REMOVE_FROM_INVENTORY":
+            return {
+            ...state,
+            inventory: [state.inventory.filter(item => item !== action.payload)]
+            }
         
         case 'RESET_INVENTORY':
             return {
