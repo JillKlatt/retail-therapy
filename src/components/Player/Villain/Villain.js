8be208f72aa90e80f01c864ddf3ca78596f4c03e
JillKlatt <klatt.jill@gmail.com>
2021-08-02 12:01:16 -0400

    Format page

--- src/components/Player/Villain/Villain.js
+++ src/components/Player/Villain/Villain.js
@@ -3,18 +3,18 @@ import './Villain.css'
 
 function Villain(props) {
 
-        return(
-            <div className='villain-container'>
-                <div className='villain-content'>
-                    <div className='villain-title'>
-                        <h3>{props.name}</h3>
-                    </div>
-                    <div className='villain-body'>
-                        <p>{props.description}</p>
-                    </div>
+    return (
+        <div className='villain-container'>
+            <div className='villain-content'>
+                <div className='villain-title'>
+                    <h3>{props.name}</h3>
+                </div>
+                <div className='villain-body'>
+                    <p>{props.description}</p>
                 </div>
             </div>
-        )
+        </div>
+    )
 
 }
 
