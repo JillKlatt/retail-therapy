af6f83f402d21eb2d5236b14662acc4a62cfa764
JillKlatt <klatt.jill@gmail.com>
2021-08-05 13:34:54 -0400

    Add bootstrap styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import reducers from './components/Reducers/index';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
