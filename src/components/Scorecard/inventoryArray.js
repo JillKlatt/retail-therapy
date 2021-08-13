f3c78965353dd994dc08c3d007ab8dc55c1077bf
JillKlatt <klatt.jill@gmail.com>
2021-08-13 15:22:22 -0400

    Change inventory array to singular

--- src/components/Scorecard/inventoryArray.js
+++ src/components/Scorecard/inventoryArray.js
@@ -1,6 +1,15 @@
 export const inventoryArray = [
-    {name: 'Off Brand Redbull',
-    inventoryHealthOutcome: +10,
-    inventoryPointsOutcome: 0
+    {
+        name: 'Off Brand Redbull',
+        inventoryDescription: "It tastes like a clopen and salty dreams but it puts a pep in your step",
+        type: 'health',
+        amount: +10
+    },
+    {
+        name: "A sense of pride",
+        inventoryDescription: "You held your ground and didn't let that customer ruin your day. You can use this item, lose your sense of pride, but gain a few points. Is it worth it?", 
+        type: 'points',
+        amount: +10
     }
 ]
+
