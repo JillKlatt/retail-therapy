76d1bc79a4b3d95ab944d27ebb3d42a81785bdab
JillKlatt <klatt.jill@gmail.com>
2021-08-13 15:29:43 -0400

    Create Modal

--- src/components/Modal/ChoiceModal.js
+++ src/components/Modal/ChoiceModal.js
@@ -1,43 +1,46 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
-import { addPoint, changeRound, changeHP, addToInventory } from '../Actions/index'
+import { changePoint, changeRound, changeHP, addToInventory } from '../Actions/index'
 import { useDispatch, useSelector } from 'react-redux';
+import './Modal.css'
 
 function ChoiceModal(props) {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const dispatch = useDispatch()
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const dispatch = useDispatch()
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const renderOutcome = () => {
-      setShow(false)
-      dispatch(changeRound())
-        if (props.answer === 'correct'){ dispatch(addPoint())}
-        if (props.hp !== undefined){ dispatch(changeHP(props.hp))}
-        if (props.inventory !== ''){ dispatch(addToInventory(props.inventory))}
-    }
-  
-    return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          {props.buttonChoice}
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          {/* <Modal.Header closeButton>
-            <Modal.Title>{props.outcome}</Modal.Title>
-          </Modal.Header> */}
-          <Modal.Body>{props.outcome}</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={renderOutcome}>
-              X
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
+  const renderOutcome = () => {
+    setShow(false)
+    dispatch(changeRound())
+    if (props.answer === 'correct') { dispatch(changePoint(10)) }
+    if (props.hp !== undefined) { dispatch(changeHP(props.hp)) }
+    if (props.inventory !== '') { dispatch(addToInventory(props.inventory)) }
   }
 
-  export default ChoiceModal
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        {props.buttonChoice}
+      </Button>
+
+      <Modal className='modal'
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered show={show} onHide={handleClose}>
+
+        <Modal.Body>{props.outcome}</Modal.Body>
+        <Modal.Footer>
+          <Button className='close-button' variant="secondary" onClick={renderOutcome}>
+            X
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ChoiceModal
