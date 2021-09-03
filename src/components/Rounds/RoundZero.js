db04228a3b8723bdd5fe9591d2221ab9f086468f
JillKlatt <klatt.jill@gmail.com>
2021-09-02 22:46:30 -0400

    Remove props from RoundZero

--- src/components/Rounds/RoundZero.js
+++ src/components/Rounds/RoundZero.js
@@ -2,11 +2,11 @@
 import Card from '../Player/Card/Card'
 import './Round.css'
 
-function RoundZero(props) {
+function RoundZero({setCurrentRound}) {
 
     return (
     <div>    
-    <Card choice={"You find yourself transported via portal to just outside your local Discount Grocery Store. By choosing one of the provided options, navigate through the store to accomplish tasks and eventually save your friends and coworkers from the Big Boss"} buttonChoice={"Begin Game!"} outcome={"Here We Go!"} inventory={''}setCurrentRound={props.setCurrentRound}/> 
+    <Card choice={"You find yourself transported via portal to just outside your local Discount Grocery Store. By choosing one of the provided options, navigate through the store to accomplish tasks and eventually save your friends and coworkers from the Big Boss"} buttonChoice={"Begin Game!"} outcome={"Here We Go!"} inventory={''}setCurrentRound={setCurrentRound}/> 
     </div>
     )
 }
