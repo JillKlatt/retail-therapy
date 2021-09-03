a2911aec65118adbefff373f770a46a6f600c5c1
JillKlatt <klatt.jill@gmail.com>
2021-09-02 22:43:57 -0400

    Clean up Card.js

--- src/components/Player/Card/Card.js
+++ src/components/Player/Card/Card.js
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux'
 import './Card.css'
 import ChoiceModal from '../../Modal/ChoiceModal'
 
-function Card(props) {
+function Card({choice, hp, answer, inventory, outcome, buttonChoice}) {
 
     const dispatch = useDispatch()
 
@@ -12,10 +12,10 @@ function Card(props) {
             <div className='card-container background'>
                 <div className='column'>
                 <div className='card-content'>
-                    <p>{props.choice}</p>
+                    <p>{choice}</p>
                 </div>
                 <div className='btn'>
-                    <ChoiceModal hp={props.hp} answer={props.answer} inventory={props.inventory} outcome={props.outcome} buttonChoice={props.buttonChoice} />
+                    <ChoiceModal hp={hp} answer={answer} inventory={inventory} outcome={outcome} buttonChoice={buttonChoice} />
                 </div>
                 </div>
             </div>
@@ -24,37 +24,3 @@ function Card(props) {
 }
 
 export default Card
-
-// class Card extends React.Component {
-
-
-//     constructor(props){
-//         super(props);
-//         this.choice = props.choice
-//         this.outcome = props.outcome
-//         this.setRound = props.setCurrentRound
-//     }
-
-//     // function changeRound() {
-//     //     this.props.setRound("2")
-//     // }
-
-
-//     renderOutcome = () => {
-//         // debugger
-//         alert(this.outcome)
-//         this.setRound(1)
-//         }
-
-//     render(){
-//         return(
-//             <div>
-//                 <button onClick={this.renderOutcome}>{this.choice}</button>
-//             </div>
-
-//         )
-//     }
-
-// }
-
-// export default Card
